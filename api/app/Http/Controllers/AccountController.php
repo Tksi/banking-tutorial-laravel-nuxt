c76aea5ac1bef5137a390799ce84bcffa17de960
Tksi <24800668+Tksi@users.noreply.github.com>
2023-03-21 19:12:42 +0900

    feat: add transactions responce

--- api/app/Http/Controllers/AccountController.php
+++ api/app/Http/Controllers/AccountController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Account;
+use App\Models\Transaction;
 
 class AccountController extends Controller
 {
     public function index($user)
     {
         // filter Account by $user
-        return Account::where('user', $user)->get();
+        $account =  Account::where('user', $user)->get();
+        // get $user's Transactions
+        $transaction = Transaction::where('user', $user)->get();
+
+        $account[0]['transactions'] = $transaction;
+        return $account[0];
     }
 
     public function store(Request $request)
